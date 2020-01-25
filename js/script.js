9aa20e29b8067b4f6d6aada9670bba218a394e87
ch0p5u3y <ch0p5u3y@localhost.localdomain>
2020-01-25 03:02:37 +0000

    added Data's challenges to site

--- js/script.js
+++ js/script.js
@@ -17,6 +17,18 @@ function tog(mode){
 	else if(mode == 6){
 		$('#sanitycheck').slideToggle('slow');
 	}
+	else if(mode == 7){
+		$('#Staff').slideToggle('slow');
+	}
+	else if(mode == 8){
+		$('#SecretAgents').slideToggle('slow');
+	}
+	else if(mode == 9){
+		$('#Encrypter').slideToggle('slow');
+	}
+	else if(mode == 10){
+		$('#TheNineties').slideToggle('slow');
+	}
 }
 
 $('#a').click(function(){tog(1); return false;});
@@ -25,3 +37,7 @@ $('#c').click(function(){tog(3); return false;});
 $('#d').click(function(){tog(4); return false;});
 $('#e').click(function(){tog(5); return false;});
 $('#f').click(function(){tog(6); return false;});
+$('#g').click(function(){tog(7); return false;});
+$('#h').click(function(){tog(8); return false;});
+$('#i').click(function(){tog(9); return false;});
+$('#j').click(function(){tog(10); return false;});
